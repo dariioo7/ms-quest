7b7d45e207bf959d4fe0b93d3206dc52b7f4b298
Dario Sagredo <dariosagredo@gmail.com>
2026-05-19 21:08:06 -0400

    Ms Quest creado

---
+++ src/main/java/cl/duoc/ms_quest/MsQuestApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_quest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsQuestApplication.class, args);
+	}
+
+}
